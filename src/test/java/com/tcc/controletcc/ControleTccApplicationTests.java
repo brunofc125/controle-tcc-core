e67a560da04c5d9a0699f716844aa9ccc44622d8
Bruno Fonseca <brunofc125@gmail.com>
2022-09-20 00:15:42 -0300

    Initial commit

---
+++ src/test/java/com/tcc/controletcc/ControleTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.controletcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
